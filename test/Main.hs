34d0bce014b16a75a5264055b77140bd17077378
Sondre Aasemoen <sondre@eons.io>
2026-03-30 22:39:42 +0200

    Fix unused imports

--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,9 @@
 module Main (main) where
 
-import Control.Exception (throw)
-import Control.Monad (void)
-import Data.Text qualified as T
 import Data.Text.Lazy (fromStrict)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import HsLua.Annotations (annotateFunction, factorial)
-import Test.Tasty (TestName, TestTree, defaultMain, testGroup)
+import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.Golden
 
 main :: IO ()
