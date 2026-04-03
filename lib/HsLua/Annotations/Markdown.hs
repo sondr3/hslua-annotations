b937e35526dce71e2dde79f450e8a52d072bfe5f
Sondre Aasemoen <sondre@eons.io>
2026-04-03 16:55:54 +0200

    Remove comments, refactor things slightly

--- lib/HsLua/Annotations/Markdown.hs
+++ lib/HsLua/Annotations/Markdown.hs
@@ -6,7 +6,6 @@ where
 
 import Data.Text (Text)
 import Data.Text qualified as T
-import Data.Text.Encoding qualified as T
 import Data.Version (showVersion)
 import HsLua.Annotations.Shared (nameToText, typeToText)
 import HsLua.Core
@@ -19,19 +18,16 @@ documentFunction = renderFunction
 documentModule :: Module a -> Text
 documentModule = renderModule
 
--- | Renders module documentation as Markdown.
 renderModule :: Module e -> Text
-renderModule mdl =
-  let fields = moduleFields mdl
-   in T.unlines
-        [ "# " <> T.decodeUtf8 (fromName $ moduleName mdl),
-          "",
-          moduleDescription mdl,
-          renderFields fields,
-          renderFunctions (moduleFunctions mdl)
-        ]
+renderModule (Module {moduleFields, moduleName, moduleDescription, moduleFunctions}) =
+  T.unlines
+    [ "# " <> nameToText moduleName,
+      "",
+      moduleDescription,
+      renderFields moduleFields,
+      renderFunctions moduleFunctions
+    ]
 
--- | Renders the full function documentation section.
 renderFunctions :: [DocumentedFunction e] -> Text
 renderFunctions = \case
   [] -> mempty
@@ -39,12 +35,7 @@ renderFunctions = \case
     "\n## Functions\n\n"
       <> T.intercalate "\n\n" (map (("### " <>) . renderFunction) fs)
 
--- | Renders documentation of a function.
-renderFunction ::
-  -- | function
-  DocumentedFunction e ->
-  -- | function docs
-  Text
+renderFunction :: DocumentedFunction e -> Text
 renderFunction fn =
   let fnDoc = functionDoc fn
       fnName = Utf8.toText $ fromName (functionName fn)
@@ -59,14 +50,12 @@ renderFunction fn =
           renderFunctionDoc fnDoc
         ]
 
--- | Renders the parameter names of a function, separated by commas.
 renderFunctionParams :: FunctionDoc -> Text
 renderFunctionParams (FunDoc {funDocParameters}) =
   T.intercalate ", " $ map param funDocParameters
   where
     param (ParameterDoc {parameterName}) = parameterName
 
--- | Render documentation for fields as Markdown.
 renderFields :: [Field e] -> Text
 renderFields fs =
   if null fs
@@ -79,17 +68,11 @@ renderFields fs =
           T.intercalate "\n" (map renderField fs)
         ]
 
--- | Renders documentation for a single field.
 renderField :: Field e -> Text
 renderField (Field {fieldName, fieldDoc}) = "| `" <> nameToText fieldName <> "` | " <> desc fieldDoc <> " |"
   where
     desc (FieldDoc {fieldDocDescription, fieldDocType}) = "`" <> typeToText fieldDocType <> "` | " <> fieldDocDescription
 
---
--- Function documentation
---
-
--- | Renders the documentation of a function as Markdown.
 renderFunctionDoc :: FunctionDoc -> Text
 renderFunctionDoc (FunDoc {funDocDescription, funDocSince, funDocParameters, funDocResults}) =
   let sinceTag = case funDocSince of
@@ -102,14 +85,11 @@ renderFunctionDoc (FunDoc {funDocDescription, funDocSince, funDocParameters, fun
         <> renderParamDocs funDocParameters
         <> renderResultsDoc funDocResults
 
--- | Renders function parameter documentation as a Markdown blocks.
 renderParamDocs :: [ParameterDoc] -> Text
 renderParamDocs pds =
   "Parameters:\n\n"
     <> T.intercalate "\n" (map renderParamDoc pds)
 
--- | Renders the documentation of a function parameter as a Markdown
--- line.
 renderParamDoc :: ParameterDoc -> Text
 renderParamDoc pd =
   mconcat
@@ -121,8 +101,6 @@ renderParamDoc pd =
       ")\n"
     ]
 
--- | Renders the documentation of a function result as a Markdown list
--- item.
 renderResultsDoc :: ResultsDoc -> Text
 renderResultsDoc = \case
   ResultsDocList [] -> mempty
@@ -130,8 +108,6 @@ renderResultsDoc = \case
     "\nReturns:\n\n" <> T.intercalate "\n" (map renderResultValueDoc rds)
   ResultsDocMult txt -> " -  " <> indent 4 txt
 
--- | Renders the documentation of a function result as a Markdown list
--- item.
 renderResultValueDoc :: ResultValueDoc -> Text
 renderResultValueDoc rd =
   mconcat
